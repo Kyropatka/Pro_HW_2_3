bf761880e612fb65e1215b5ca899d1f2fbabbab5
Denys <70828327+Kyropatka@users.noreply.github.com>
2023-11-25 15:31:00 +0200

    Add files via upload

---
+++ Pro_HW_2_3/src/Save.java
@@ -0,0 +1,34 @@
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface Save {
+}
+
+class Test {
+    @Save
+    public int a;
+    @Save
+    public String b;
+
+    public int getA() {
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" + "a=" + a + ", b='" + b + '\'' + '}';
+    }
+}
